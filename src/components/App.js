0606a9b72027914ed550d66588ff8cb46a0e011b
weillercarvalho <engweiller@hotmail.com>
2022-10-19 01:53:00 -0300

    feat [] Added all initial libs.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { Global } from "./Global";
+import {Reset} from "styled-reset";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+export default function App() {
+    return (
+        <>
+            <Reset />
+            <Global />
+            <BrowserRouter>
+                <Routes>
+                    <Route />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
---
+++ src/components/Global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+    *{
+        box-sizing:border-box;
+    }
+    body {
+        width: 100vw;
+        height: 100vh;
+        font-family: 'Lato', sans-serif;
+        font-family: 'Oswald', sans-serif;
+        font-family: 'Passion One', cursive;
+    }
+    @media (max-width: 375px) {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export { Global };
